a0ac7a93cf4393ccfdc1c7e9934085c0cf97733e
João marcos <lourencosjm41@gmail.com>
2020-02-27 19:24:58 -0300

    Mask

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  host: "192.168.0.104", //"10.224.68.73",
+  host: "10.224.68.73",
   username: "postgres",
   password: "postgres",
   database: "Project",
